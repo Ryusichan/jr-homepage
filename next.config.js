8a551ce6b4d6d11fe9948788c5a2e25fa2448b32
Ryusichan <Ryusichan@github.com>
2022-10-06 16:11:11 +0900

    nextjs export setting

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ const nextConfig = {
   images: {
     domains: ["source.unsplash.com"],
   },
+  trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/p/hello-nextjs': { page: '/pricing', query: { title: 'hello-nextjs' } },
+      '/p/learn-nextjs': { page: '/whypluton', query: { title: 'learn-nextjs' } },
+      '/p/deploy-nextjs': { page: '/whyJR', query: { title: 'deploy-nextjs' } },
+    }
+  },
 };
 
 module.exports = nextConfig;
