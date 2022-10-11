45134c79e257a9312ab02eee802f456f259e823b
Ryusichan <Ryusichan@github.com>
2022-10-11 18:08:04 +0900

    bug fix

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ export default function Document() {
         />
         <meta charset="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
       </Head>
       <body>
         <Main />
